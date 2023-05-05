1fdd2abbc159c7e5a31e13b9ba8fbe913c053978
dimpram101 <dimas2004211@gmail.com>
2023-05-06 00:03:52 +0800

    add autentikasi user

--- backend/src/middlewares/verifyToken.js
+++ backend/src/middlewares/verifyToken.js
@@ -14,3 +14,38 @@ export const verifyToken = (req, res, next) => {
     return res.status(403).json({ status: "ERROR", msg: "Token tidak valid!"})
   }
 }
+
+
+// function verifyToken(req, res, next) {
+//   const token = req.cookies.jwt;
+//   if (!token) {
+//     return res.status(401).json({ message: 'Token is missing' });
+//   }
+
+//   try {
+//     const decodedToken = jwt.verify(token, secretKey);
+//     req.currentUser = decodedToken;
+//     next();
+//   } catch (err) {
+//     if (err instanceof jwt.TokenExpiredError) {
+//       const refreshToken = req.cookies.refreshToken;
+//       if (!refreshToken) {
+//         return res.status(401).json({ message: 'Refresh token is missing' });
+//       }
+
+//       try {
+//         const decodedRefreshToken = jwt.verify(refreshToken, secretKey);
+//         const newAccessToken = generateAccessToken(decodedRefreshToken.user);
+//         res.cookie('jwt', newAccessToken, { httpOnly: true, secure: true });
+//         req.currentUser = decodedRefreshToken.user;
+//         next();
+//       } catch (err) {
+//         return res.status(401).json({ message: 'Refresh token is invalid' });
+//       }
+//     } else {
+//       return res.status(401).json({ message: 'Token is invalid' });
+//     }
+//   }
+// }
+
+// app.use(verifyToken);
